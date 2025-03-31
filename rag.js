b3e599fec5832b07a0a64249533d6b92b9b46edf
Andy <andysexpeditions@gmail.com>
2025-03-31 15:43:42 +0200

    proxy support, to be tested

--- rag.js
+++ rag.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 import * as fs from "node:fs";
 
+import { HttpsProxyAgent } from "https-proxy-agent";
 import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
@@ -13,13 +14,20 @@ import {
 } from "@langchain/core/runnables";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { Document } from "@langchain/core/documents";
 
 // Check if OpenAI API key is available
 if (!process.env.OPENAI_API_KEY) {
   console.error("OPENAI_API_KEY is not set in .env file");
   process.exit(1);
 }
+
+// For self-signed certifiactes in the SSL chain
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+// Define proxy URL
+const proxyUrl = `http://${process.env.PROXY_USERNAME}:${process.env.PROXY_PASSWORD}@webproxy.prod.d003.loc:8080`;
+// Create proxy agents for HTTP and HTTPS
+const proxyAgent = new HttpsProxyAgent(proxyUrl);
 
 // Helper function to format documents as a string
 const formatDocumentsAsString = (documents) => {
@@ -34,6 +42,11 @@ async function runRAG() {
     // Initialize the LLM to use to answer the question
     const model = new ChatOpenAI({
       model: "gpt-4o",
+      configuration: {
+        // apiKey: process.env.OPENAI_API_KEY,
+        // baseURL: "https://api.openai.com/v1",
+        httpAgent: proxyAgent
+      }
     });
     
     console.log("Loading and processing text...");
@@ -47,7 +60,13 @@ async function runRAG() {
     console.log("Creating vector store from documents...");
     const vectorStore = await MemoryVectorStore.fromDocuments(
       docs,
-      new OpenAIEmbeddings()
+      new OpenAIEmbeddings({
+        configuration: {
+          // apiKey: process.env.OPENAI_API_KEY,
+          // baseURL: "https://api.openai.com/v1",
+          httpAgent: proxyAgent
+        }
+      })
     );
     
     // Initialize a retriever wrapper around the vector store
